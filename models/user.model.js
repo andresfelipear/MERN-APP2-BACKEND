ae887fe459d0c2897049dc1aee0132fbebe9e58c
Andres Arevalo <andresfelipear@gmail.com>
2022-03-24 17:44:49 -0700

    user model, token model (for forgot password) done

---
+++ models/user.model.js
@@ -0,0 +1,39 @@
+const passportLocalMongoose = require('passport-local-mongoose')
+const {Schema, model} = require("../db/connection") 
+
+const Session = new Schema({
+    refreshToken: {
+        type: String,
+        default: ''
+    }
+})
+
+const UserSchema = new Schema({
+    username: {type: String, unique: true, required: true},
+    email: {type: String, unique: true},
+    icon: {type: String},
+    password: {type: String},
+    authStrategy: {
+        type:String,
+        default: 'local'
+    },
+    refreshToken: {
+        type: [Session]
+    },
+})
+
+
+
+//remove refreshtoken from the response
+UserSchema.set("toJSON", {
+    transform: function(doc, ret, options){
+        delete ret.refreshToken
+        return ret
+    }
+})
+
+UserSchema.plugin(passportLocalMongoose)
+
+const User = model("User", UserSchema)
+
+module.exports = User
