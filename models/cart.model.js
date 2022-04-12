4ae65d4999abb62fcb000ff9345a59ca3d0705de
Andres Arevalo <andresfelipear@gmail.com>
2022-04-11 18:06:02 -0700

    cart model done

---
+++ models/cart.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const cartSchema = new Schema({
+    user:{
+        type: Schema.Types.ObjectId, ref: 'User' 
+    },
+    products:[{
+        product: { 
+            type: Schema.Types.ObjectId,
+            ref: 'Breakfasts'
+        },
+        quantity:{
+            type: Number,
+            defaul:0
+        },
+        price:{
+            type: Number,
+            default: 0
+        }
+    }],
+    totalPrice:{
+        type: Number,
+        default:function(){
+            let total=0;
+            for(let i=0;i<this.products.length;i++){
+                total+= this.products[i].price
+            }
+            return total
+        }
+    }
+    
+})
+
+module.exports = mongoose.model('Cart', cartSchema)
